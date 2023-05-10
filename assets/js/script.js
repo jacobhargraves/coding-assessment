ab75716e46d7f282fba0880fbfbe0a8a584c8388
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-05-10 09:59:05 -0600

    added some features to display if the selected answer is correct or not as well as dispaly a score.  Needing to fix a bug because it skips over questions 3 and 5

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,8 @@ const questionText = document.querySelector(".questionText");
 const options = document.querySelector(".optionsList");
 const timerCountDown = document.querySelector(".timerCountDown");
 const nextBtn = document.querySelector(".nextBtn");
+const evaluate = document.querySelector(".evaluate");
+const scoreDiv = document.querySelector(".score");
 
 // begins quiz and hides startBoxContainer
 startBtn.addEventListener("click" , function () {
@@ -20,9 +22,11 @@ var questionNumber = 1;
 
 // function to show questions when startBtn is clicked
 function showQuetions(index) {
+    let scoreTag = '<div class="scoreDisplay">' + 'Score: ' + score + '</div>';
+    scoreDiv.innerHTML = scoreTag;
     let questionTag = '<h2>'+ questions[index].numb + ". " + questions[index].question +'</h2>';
     questionText.innerHTML = questionTag;
-    let optionTag = '<button class="choice">'+ questions[index].choices[0] +'</button>'
+    let optionTag = '<div class="choice">'+ questions[index].choices[0] +'</div>'
     + '<div class="choice">'+ questions[index].choices[1] +'</div>'
     + '<div class="choice">'+ questions[index].choices[2] +'</div>'
     + '<div class="choice">'+ questions[index].choices[3] +'</div>';
@@ -34,7 +38,7 @@ function showQuetions(index) {
     const choice = options.querySelectorAll(".choice");
 
     for(i=0; i < choice.length; i++){
-       choice[i].setAttribute("onclick", "optionSelected(this)");
+       choice[i].setAttribute("onclick", "evaluateAnswer(this)");
     }
 
     nextBtn.addEventListener('click', function (){
@@ -42,6 +46,7 @@ function showQuetions(index) {
             questionCount++;
             questionNumber++;
             showQuetions(questionCount);
+            evaluate.classList.add("hidden"); // hides if answer is correct or incorrect when pressed
         }
     })
 }
@@ -51,7 +56,7 @@ function showQuetions(index) {
 // hides contentContainer and shows time up
 function sendMessage() {
     // contentContainer.classList.add("hidden");
-    alert('Time Up!');
+    alert('Time is Up!');
 }
 
 //starts timer at 60 seconds and refreshes every second
@@ -67,5 +72,24 @@ function startTimer() {
             sendMessage();
           }
 
-    }, 1000)
+    }, 1000);
+}
+
+var score = 0;
+
+function evaluateAnswer(answer) {
+    var answerInput = answer.textContent;
+    var correctAnswer = questions[questionCount].answer;
+    // var eachOption = options.children.length;
+
+    if (answerInput == correctAnswer) {
+        score += 1;
+        let evaluateTag = "<h3 id='correct'>Correct!<h3>";
+        evaluate.innerHTML = evaluateTag;
+    }
+    else {
+        let evaluateTag = "<h3 id='incorrect'>Incorrect ☹️<h3>";
+        evaluate.innerHTML = evaluateTag;
+    }
+    evaluate.classList.remove("hidden"); // removes hidden css class so evaluation can be displayed
 }
